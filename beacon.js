755d58eb1ae6d1aabfe659d0343d83ab45f40886
Nick Floyd <nickfloyd@newrelic.com>
2015-03-12 21:42:25 -0700

    updates the beacon with the ability to accept parameters

--- beacon.js
+++ beacon.js
@@ -1,22 +1,29 @@
-var http, gpio, gu
-var intervalInMills = 100000, setStatusBeacon, insightsRequest, query, fullPath;
-var RED_PIN = 11, GREEN_PIN = 13, BLUE_PIN = 15;
-
-gpio = require("pi-gpio");
+var http, gu, setStatusBeacon
 http = require('http');
 gu = require('./gpio_utils')
 
-query = "?nrql=SELECT%20count(exception)%20FROM%20NewRelic_AzurePortal_APM_Requests%20WHERE%20application%20LIKE%20'NR-Stamp%25'%20AND%20exception%20IS%20NOT%20NULL%20SINCE%201%20day%20ago%20limit%2050";
-fullPath = '/v1/accounts/429813/query' + query
+// node beacon.js HrurPdQHISZ4ESs8iydk34u7tKHv1zXU 429813 staging-insights-api.newrelic.com
+var insightsApiKey = !process.argv[2] ? throw ({'error':'Please supply an New Relic Insights query key'}) : process.argv[2]; //ex.HrurPdQHISZ4ESs8iydk34u7tKHv1zXU
+var newrelicAccountId = !process.argv[3] ? throw ({'error':'Please supply an New Relic account Id'}) : process.argv[3]; //ex.429813
+var insightsHost = !process.argv[4] ? "insights-api.newrelic.com" : process.argv[4]; //ex.staging-insights-api.newrelic.com
 
-insightsRequest = {
-  host: 'staging-insights-api.newrelic.com',
+//RGB pins for a rev 2 raspberry pi (rev 1 are RED_PIN = 17, GREEN_PIN = 27, BLUE_PIN = 22;)
+var RED_PIN = 11, GREEN_PIN = 13, BLUE_PIN = 15;
+
+//Service polling ineterval in Milliseconds
+var intervalInMills = 100000;
+//NRQL query URL encoded from https://insights.newrelic.com/accounts/[Your_Account]/manage/api_keys
+var query = "SELECT%20count(exception)%20FROM%20NewRelic_AzurePortal_APM_Requests%20WHERE%20application%20LIKE%20'NR-Stamp%25'%20AND%20exception%20IS%20NOT%20NULL%20SINCE%201%20day%20ago%20limit%2050";
+var fullPath = "/v1/accounts/" + newrelicAccountId + "/query?nrql=" + query
+
+var insightsRequest = {
+  host: '' + insightsHost + '',
   port: 80,
   path: fullPath,
   method: 'GET',
   headers: {
     'Accept:': 'application/json',
-    'X-Query-Key': 'HrurPdQHISZ4ESs8iydk34u7tKHv1zXE'
+    'X-Query-Key': '' + insightsApiKey + ''
   }
 };
 
@@ -34,6 +41,7 @@ setStatusBeacon = function() {
     });
 
     return res.on('end', function() {
+      //TODO: determine why undefined is in the data stream
       data = data.replace("undefined","")
       results = JSON.parse(data).results[0].count;
       
@@ -49,7 +57,7 @@ setStatusBeacon = function() {
     });
 
   }).on('error', function(e) {
-      console.log("Got error: ", e);
+      console.log("error: ", e);
       gu.writePin(BLUE_PIN); 
 });
 
