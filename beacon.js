fa75ba5b13a2befbab9d369e17d8e5a139ba10e9
Nick Floyd <nicholas.floyd.info@gmail.com>
2015-03-12 21:52:44 -0700

    updates beacon to throw if params are missing

--- beacon.js
+++ beacon.js
@@ -2,16 +2,27 @@ var http, gu, setStatusBeacon
 http = require('http');
 gu = require('./gpio_utils')
 
+var insightsApiKey, newrelicAccountId, insightsHost;
+
 // node beacon.js HrurPdQHISZ4ESs8iydk34u7tKHv1zXU 429813 staging-insights-api.newrelic.com
-var insightsApiKey = !process.argv[2] ? throw ({'error':'Please supply an New Relic Insights query key'}) : process.argv[2]; //ex.HrurPdQHISZ4ESs8iydk34u7tKHv1zXU
-var newrelicAccountId = !process.argv[3] ? throw ({'error':'Please supply an New Relic account Id'}) : process.argv[3]; //ex.429813
-var insightsHost = !process.argv[4] ? "insights-api.newrelic.com" : process.argv[4]; //ex.staging-insights-api.newrelic.com
+if(!process.argv[2]){
+	throw ({'error':'Please supply an New Relic Insights query key'});
+}
+insightsApiKey = process.argv[2]; //ex.HrurPdQHISZ4ESs8iydk34u7tKHv1zXU
+
+if(!process.argv[3]){
+	throw ({'error':'Please supply an New Relic account Id'});
+}
+newrelicAccountId = process.argv[3]; //ex.429813
+
+insightsHost = !process.argv[4] ? "insights-api.newrelic.com" : process.argv[4]; //ex.staging-insights-api.newrelic.com
 
 //RGB pins for a rev 2 raspberry pi (rev 1 are RED_PIN = 17, GREEN_PIN = 27, BLUE_PIN = 22;)
 var RED_PIN = 11, GREEN_PIN = 13, BLUE_PIN = 15;
 
 //Service polling ineterval in Milliseconds
 var intervalInMills = 100000;
+
 //NRQL query URL encoded from https://insights.newrelic.com/accounts/[Your_Account]/manage/api_keys
 var query = "SELECT%20count(exception)%20FROM%20NewRelic_AzurePortal_APM_Requests%20WHERE%20application%20LIKE%20'NR-Stamp%25'%20AND%20exception%20IS%20NOT%20NULL%20SINCE%201%20day%20ago%20limit%2050";
 var fullPath = "/v1/accounts/" + newrelicAccountId + "/query?nrql=" + query
